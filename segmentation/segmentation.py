e85a61b87d4373e2222325abab77e826d7c2d73e
hson19 <hadriensonnet73@gmail.com>
2024-10-06 10:54:44 +0000

    cuda print

--- segmentation/segmentation.py
+++ segmentation/segmentation.py
@@ -23,7 +23,10 @@ cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 predictor = DefaultPredictor(cfg)
+
+print(f"Model is running on: {cfg.MODEL.DEVICE}")
 
 class ImageRequest(BaseModel):
     image: str
